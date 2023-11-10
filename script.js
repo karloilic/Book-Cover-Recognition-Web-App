1cfffb0ecf008a621157e607d8853696040e2ea1
karlo <uu.ceif@gmail.com>
2023-11-10 12:17:22 +0100

    add comments

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-// Define the common URL prefix and suffix format
+// Define the URL prefix and suffix format
 const urlPrefix = 'https://example.com/image';
 const urlSuffixFormat = '00000';
 
 // Arrays to store the fetched data
+// Adjust based on the data which should be fetched
 const imageDataArray = [];
 const data1Array = [];
 const data2Array = [];
@@ -12,13 +13,14 @@ function fetchData(url, dataArray) {
   return fetch(url)
     .then(response => {
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error('Network response negative');
       }
-      return response.text(); // Adjust based on the type of data (text, JSON, etc.)
+      return response.text(); // Adjust based on type of data (text, JSON, etc.)
     })
     .then(data => {
       dataArray.push(data);
       if (dataArray.length === 999999) {
+        // Adjust the length
         // All data has been fetched
         // Display data in UI
         displayDataInUI(imageDataArray, data1Array, data2Array);
@@ -29,7 +31,7 @@ function fetchData(url, dataArray) {
     });
 }
 
-// Loop through the range of numerical values and fetch all types of data
+// Loop through the range of numerical values and fetch all chosen data
 for (let i = 0; i <= 999999; i++) {
   const urlSuffix = urlSuffixFormat + i.toString().padStart(5, '0');
   const imageUrl = urlPrefix + urlSuffix;
@@ -43,7 +45,7 @@ for (let i = 0; i <= 999999; i++) {
 
 function createImageElement(imageData) {
     const imageElement = document.createElement('img');
-    const blob = new Blob([imageData], { type: 'image/jpeg' }); // Adjust the MIME type if needed
+    const blob = new Blob([imageData], { type: 'image/jpeg' }); // Adjust MIME type if needed
     const imageUrl = URL.createObjectURL(blob);
     imageElement.src = imageUrl;
     return imageElement;
@@ -51,17 +53,17 @@ function createImageElement(imageData) {
 
 // Function to display the data in the UI
 function displayDataInUI(images, data1, data2) {
-  const imageContainer = document.getElementById('image-container'); // Replace with your container element ID
-  const data1Container = document.getElementById('data1-container'); // Replace with your container element ID
-  const data2Container = document.getElementById('data2-container'); // Replace with your container element ID
+  const imageContainer = document.getElementById('image-container'); // Replace string with your container element ID
+  const data1Container = document.getElementById('data1-container'); // Replace string with your container element ID
+  const data2Container = document.getElementById('data2-container'); // Replace string with your container element ID
 
-  // Display images
+  // Display images in UI
   images.forEach(imageData => {
     const imageElement = createImageElement(imageData);
     imageContainer.appendChild(imageElement);
   });
 
-  // Display data1 and data2
+  // Display data1 and data2 in UI
   data1.forEach(data => {
     const data1Element = document.createElement('div');
     data1Element.textContent = data;
